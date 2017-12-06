66998cbb9a7f8412fdc545e444ff2a9cfd4d1068
Ciprian Radu <ciprian_radu@yahoo.com>
2017-12-06 15:05:19 +0200

    module 4 solution

---
+++ module54-solution/src/routes.js
@@ -0,0 +1,47 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/menu/templates/home.template.html'
+  })
+
+  // Premade list page
+  .state('categories', {
+    url: '/categories',
+    templateUrl: 'src/menu/templates/categories.template.html',
+    controller: 'MenuCategoriesController',
+    resolve: {
+      categories: ['MenuDataService', function (MenuDataService) {
+        return MenuDataService.getAllCategories();
+      }]
+    }
+  });
+
+  // Premade list page
+  .state('items', {
+    url: '/items?category={category}',
+    templateUrl: 'src/menu/templates/items.template.html',
+    controller: 'MenuDataController',
+    resolve: {
+      items: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataService) {
+        return MenuDataService.getAllItemsForCategory($stateParams.category);
+      }]
+    }
+  });
+}
+
+})();
